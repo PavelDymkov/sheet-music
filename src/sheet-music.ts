2bf7b996aac0bbc8600c44c9fa507bd9f8aa4617
Pavel Dymkov <dymkov86@gmail.com>
2021-02-23 18:28:55 +0300

    sheet-music

--- src/sheet-music.ts
+++ src/sheet-music.ts
@@ -2,8 +2,11 @@ import { EventEmitter } from "eventemitter3";
 import { not } from "logical-not";
 
 import { Bar } from "./bar";
+import { Note } from "./note";
+import { NoteValue } from "./note-value";
 import { Specifier } from "./specifiers/specifier";
-import { Staff } from "./staff";
+import { Staff, Cursor as StaffCursor } from "./staff";
+import { Fraction } from "./tools/fraction";
 
 const staffs = Symbol();
 const staffIndex = Symbol();
@@ -12,12 +15,13 @@ const specifiers = Symbol();
 
 export class SheetMusic {
     static readonly Events = {
-        change: Symbol(),
+        Change: Symbol("change"),
+        ChangeNote: Symbol("change note"),
+        CursorMove: Symbol("cursor move"),
     };
 
+    readonly events = new EventEmitter<symbol>();
     readonly cursor: Cursor;
-    readonly bars = createBarsIterator(this);
-    readonly events = new EventEmitter();
 
     readonly [staffs]: Staff[] = [];
     readonly [bars]: Bar[][] = [];
@@ -30,22 +34,73 @@ export class SheetMusic {
         this.cursor[owner] = this;
     }
 
+    @Event(SheetMusic.Events.Change)
     insertStaff(): void {
         this[staffs].splice(++this[staffIndex], 0, new Staff());
     }
 
+    @Event(SheetMusic.Events.Change)
     removeStaff(): void {
         if (this[staffIndex] !== -1) {
             this[staffs].splice(this[staffIndex]--, 1);
         }
     }
+
+    @StaffProcessor<NoteValue>(
+        (staff, noteValue) => staff.insert(noteValue),
+        (cursor, diff) => cursor.forward(diff),
+    )
+    @Event(SheetMusic.Events.Change)
+    insert(noteValue: NoteValue): void {}
+
+    @StaffProcessor<NoteValue>(
+        (staff, nextNoteValue) => staff.changeNoteValue(nextNoteValue),
+        (cursor, diff) => cursor.forward(diff),
+    )
+    @Event(SheetMusic.Events.Change)
+    changeNoteValue(nextNoteValue: NoteValue): void {}
+
+    @StaffProcessor(
+        staff => staff.insertIrregularRhythm(),
+        (cursor, diff) => cursor.forward(diff),
+    )
+    @Event(SheetMusic.Events.Change)
+    insertIrregularRhythm(): void {}
+
+    @StaffProcessor(
+        staff => staff.remove(),
+        (cursor, diff) => cursor.backward(diff),
+    )
+    @Event(SheetMusic.Events.Change)
+    remove(): void {}
+
+    @Event(SheetMusic.Events.ChangeNote)
+    insertNote(note: Note): void {
+        if (this[staffIndex] !== -1) {
+            this[staffs][this[staffIndex]].insertNote(note);
+        }
+    }
+
+    @Event(SheetMusic.Events.ChangeNote)
+    removeNote(note: Note): void {
+        if (this[staffIndex] !== -1) {
+            this[staffs][this[staffIndex]].removeNote(note);
+        }
+    }
+
+    bars({ offset = 0 }: { offset: number }): Generator {
+        return barsIterator(this);
+    }
 }
+
+function* barsIterator(sheetMusic: SheetMusic): Generator {}
 
 const owner = Symbol();
 
 export class Cursor {
     [owner]: SheetMusic;
 
+    @Event(SheetMusic.Events.CursorMove)
     next(): void {
         if (this[owner][staffIndex] === -1) {
             const diff = this[owner][staffs][
@@ -60,6 +115,7 @@ export class Cursor {
         }
     }
 
+    @Event(SheetMusic.Events.CursorMove)
     prev(): void {
         if (this[owner][staffIndex] == -1) {
             const diff = this[owner][staffs][
@@ -74,46 +130,61 @@ export class Cursor {
         }
     }
 
+    @Event(SheetMusic.Events.CursorMove)
     nextStaff(): void {
         const i = this[owner][staffIndex] + 1;
 
         if (this[owner][staffs][i]) this[owner][staffIndex] = i;
     }
 
+    @Event(SheetMusic.Events.CursorMove)
     prevStaff(): void {
         const i = this[owner][staffIndex] - 1;
 
         if (this[owner][staffs][i]) this[owner][staffIndex] = i;
     }
 
+    @Event(SheetMusic.Events.CursorMove)
     nextVoice(): void {
         not(this[owner][staffs][this[owner][staffIndex]].cursor.nextVoice()) &&
             this.nextStaff();
     }
 
+    @Event(SheetMusic.Events.CursorMove)
     prevVoice(): void {
         not(this[owner][staffs][this[owner][staffIndex]].cursor.prevVoice()) &&
             this.prevStaff();
     }
 }
 
-function createBarsIterator(sheetMusic: SheetMusic) {
-    return {
-        *[Symbol.iterator]() {
-            let i = 0;
+function Event(type: symbol): MethodDecorator {
+    return (target: any, propertyKey: string | symbol) => {
+        const origin = target[propertyKey] as Function;
 
-            while (i < sheetMusic[bars].length) yield sheetMusic[bars][i++];
-        },
+        return {
+            value(this: SheetMusic, args: any[]): void {
+                origin.apply(this, args);
+
+                this.events.emit(type);
+            },
+        } as TypedPropertyDescriptor<any>;
     };
 }
 
-function emitChanges(sheetMusic: SheetMusic, index: number) {
-    sheetMusic.events.emit(SheetMusic.Events.change, {
-        index,
-        changedBars: function* () {
-            for (let bars of sheetMusic.bars) {
-                yield bars;
-            }
-        },
-    });
+function StaffProcessor<T = void>(
+    staffHandler: (staff: Staff, arg: T) => Fraction,
+    diffHandler: (cursor: StaffCursor, diff: Fraction) => void,
+): MethodDecorator {
+    return () => {
+        return {
+            value(this: SheetMusic, arg: T): void {
+                const staff = this[staffs][this[staffIndex]];
+                const diff = staffHandler(staff, arg);
+
+                this[staffs].forEach(item => {
+                    if (item !== staff) diffHandler(item.cursor, diff);
+                });
+            },
+        } as TypedPropertyDescriptor<any>;
+    };
 }
--- src/staff.ts
+++ src/staff.ts
@@ -1,5 +1,6 @@
+import { Note } from "./note";
 import { NoteValue } from "./note-value";
-import { Part } from "./part";
+import { Part, Cursor as PartCursor } from "./part";
 import { Fraction } from "./tools/fraction";
 
 const main = Symbol();
@@ -8,7 +9,7 @@ const voice = Symbol();
 const dependent = Symbol();
 
 export class Staff {
-    readonly cursor = new StaffCursor(this);
+    readonly cursor = Object.assign(new Cursor(), { [staff]: this }) as Cursor;
 
     [main]: Part = new Part();
     [extra]: Part = new Part();
@@ -16,56 +17,64 @@ export class Staff {
     [voice]: Part = this[main];
     [dependent]: Part = this[extra];
 
+    @SideEffect(dependentCursorForward)
     insert(noteValue: NoteValue): Fraction {
-        const diff = this[voice].insert(noteValue);
-
-        this[dependent].cursor.forward(diff);
-
-        return diff;
+        return this[voice].insert(noteValue);
     }
 
+    @SideEffect(dependentCursorForward)
+    changeNoteValue(nextNoteValue: NoteValue): Fraction {
+        return this[voice].changeNoteValue(nextNoteValue);
+    }
+
+    @SideEffect(dependentCursorForward)
+    insertIrregularRhythm(): Fraction {
+        return this[voice].insertIrregularRhythm();
+    }
+
+    @SideEffect(dependentCursorBackward)
     remove(): Fraction {
-        const diff = this[voice].remove();
+        return this[voice].remove();
+    }
 
-        this[dependent].cursor.backward(diff);
+    insertNote(note: Note): void {
+        this[voice].insertNote(note);
+    }
 
-        return diff;
+    removeNote(note: Note): void {
+        this[voice].removeNote(note);
     }
 }
 
-export class StaffCursor {
-    constructor(private staff: Staff) {}
+const staff = Symbol();
+
+export class Cursor {
+    [staff]: Staff;
 
     forward(delta: Fraction): void {
-        this.staff[main].cursor.forward(delta);
-        this.staff[extra].cursor.forward(delta);
+        this[staff][main].cursor.forward(delta);
+        this[staff][extra].cursor.forward(delta);
     }
 
     backward(delta: Fraction): void {
-        this.staff[main].cursor.backward(delta);
-        this.staff[extra].cursor.backward(delta);
+        this[staff][main].cursor.backward(delta);
+        this[staff][extra].cursor.backward(delta);
     }
 
+    @SideEffect(dependentCursorForward)
     next(): Fraction {
-        const diff = this.staff[voice].cursor.next();
-
-        this.staff[dependent].cursor.forward(diff);
-
-        return diff;
+        return this[staff][voice].cursor.next();
     }
 
+    @SideEffect(dependentCursorBackward)
     prev(): Fraction {
-        const diff = this.staff[voice].cursor.prev();
-
-        this.staff[dependent].cursor.backward(diff);
-
-        return diff;
+        return this[staff][voice].cursor.prev();
     }
 
     nextVoice(): boolean {
-        if (this.staff[voice] === this.staff[main]) {
-            this.staff[voice] = this.staff[extra];
-            this.staff[dependent] = this.staff[main];
+        if (this[staff][voice] === this[staff][main]) {
+            this[staff][voice] = this[staff][extra];
+            this[staff][dependent] = this[staff][main];
 
             return true;
         }
@@ -73,13 +82,49 @@ export class StaffCursor {
         return false;
     }
     prevVoice(): boolean {
-        if (this.staff[voice] === this.staff[extra]) {
-            this.staff[voice] = this.staff[main];
-            this.staff[dependent] = this.staff[extra];
+        if (this[staff][voice] === this[staff][extra]) {
+            this[staff][voice] = this[staff][main];
+            this[staff][dependent] = this[staff][extra];
 
             return true;
         }
 
         return false;
     }
+}
+
+function SideEffect(
+    action: (cursor: PartCursor, diff: Fraction) => void,
+): MethodDecorator {
+    return (target: any, propertyKey: string | symbol) => {
+        const origin = target[propertyKey] as (
+            this: Staff | Cursor,
+            ...args: any[]
+        ) => Fraction;
+
+        return {
+            value(this: Staff | Cursor, ...args: any[]): Fraction {
+                const diff = origin.apply(this, args);
+
+                switch (true) {
+                    case this instanceof Staff:
+                        action((this as Staff)[dependent].cursor, diff);
+                        break;
+                    case this instanceof Cursor:
+                        action((this as Cursor)[staff][dependent].cursor, diff);
+                        break;
+                }
+
+                return diff;
+            },
+        } as TypedPropertyDescriptor<any>;
+    };
+}
+
+function dependentCursorForward(cursor: PartCursor, diff: Fraction) {
+    cursor.forward(diff);
+}
+
+function dependentCursorBackward(cursor: PartCursor, diff: Fraction) {
+    cursor.backward(diff);
 }
